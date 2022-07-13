f38dcbf9fa6e0e22f39a86a5da993e2ab98f624b
KE422 <76940319+KE422@users.noreply.github.com>
2022-07-13 12:04:29 +0900

    Try for & if

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Doctor Strange'},
+    { id: 12, name: 'Iron Man'},
+    { id: 13, name: 'Captain America'},
+    { id: 14, name: 'Hulk'},
+    { id: 15, name: 'Spider-Man'},
+    { id: 16, name: 'Wanda'},
+    { id: 17, name: 'Thor'},
+    { id: 18, name: 'Black Widow'},
+    { id: 19, name: 'Rocket'},
+    { id: 20, name: 'Gamora'},
+]
